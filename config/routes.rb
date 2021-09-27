f6228268a4dda8993eb781536149f100cd316a02
Tichelrahk <ebovet@gmail.com>
2021-09-28 03:31:41 +1000

    set up controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'sales#index'
   get "about", to: "sales#about"
-  resources :sales, only: %i[new create]
+  resources :sales, only: %i[new create edit]
+  resources :products, only: %i[index new create edit]
 end
