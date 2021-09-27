ba0f948421071a259ed72063fa3e41604cf72aea
Tichelrahk <ebovet@gmail.com>
2021-09-28 04:00:55 +1000

    added pages for the routes:

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -11,8 +11,26 @@ class SalesController < ApplicationController
 	end
 
 	def create
+	    @sale = Sale.new(sale_params)
+	    if @sale.save
+	      redirect_to root_path
+	    else
+	      render :new
+	    end
 	end
 
 	def edit
 	end
+
+	private
+
+	def set_sale
+    	@sale = Sale.find(params[:id])
+    end
+
+    # Needs work
+    def sale_params
+    	params.require(:sale).permit(:name, :description, :photo)
+    end
+  
 end
