580945a9141d4da6fb3d6259ed8eeb853a06111c
Tichelrahk <ebovet@gmail.com>
2021-09-28 03:18:49 +1000

    added db functionality for sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,12 @@
 class Sale < ApplicationRecord
 	has_many :sale_items
 	has_many :products, through: :sale_items
+
+	def total_price
+  		sale_items.sum { |item| (item.product.price * item.quantity) }
+	end
+
+	def money_format(cents)
+		sprintf('%.2f', (cents/100).to_f)
+	end
 end
