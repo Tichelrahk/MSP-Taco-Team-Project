c9e7ad72dc05322c6eadfc8d178d40261578d1fd
Tichelrahk <ebovet@gmail.com>
2021-09-28 12:44:09 +1000

    crud the products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,46 @@
 class ProductsController < ApplicationController
 	def index
-		@products = Product.all
+		@products = Product.where("active = true")
 	end
 
 	def create
+		price = product_params[:price]
+	    @product = Product.new(product_params)
+	    @product.active = true
+	    price = (price.to_f * 100).to_i
+	    @product.price = price
+	    if @product.save
+	      redirect_to products_path
+	    else
+	      render :new
+	    end
 	end
 
 	def new
 	end
 
 	def edit
+		@product = Product.find(params[:id])
+		@product.price = sprintf('%.2f', (@product.price/100).to_f)
 	end
+
+	def update
+		@product = Product.find(params[:id])
+		h = product_params.to_h
+		price = h[:price]
+		price = (price.to_f * 100).to_i
+		h[:price] = price
+		puts h
+		if @product.update(h)
+	      redirect_to products_path
+	    else
+	      render :edit
+	    end
+	end
+
+	private
+
+    def product_params
+    	params.require(:product).permit(:name, :stock, :price, :active)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'sales#index'
   get "about", to: "sales#about"
-  resources :sales, only: %i[new create edit]
-  resources :products, only: %i[index new create edit]
+  resources :sales, only: %i[new create edit update]
+  resources :products, only: %i[index new create edit update]
 end
