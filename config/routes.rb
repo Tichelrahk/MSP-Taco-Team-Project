74020f1bb03231ac76525c7d4540bcfbd9bdb54b
Tichelrahk <ebovet@gmail.com>
2021-09-28 19:04:31 +1000

    added weekly and monthly reports

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'sales#index'
   get "about", to: "sales#about"
+  get "weekly-reports", to: "sales#weekly"
+  get "monthly-reports", to: "sales#monthly"
   resources :sales, only: %i[new create edit update]
   resources :products, only: %i[index new create edit update]
 end
