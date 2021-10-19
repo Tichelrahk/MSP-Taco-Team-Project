ecdfe55f2e3aad0e40155e9f962ddd0820f33707
Tichelrahk <ebovet@gmail.com>
2021-10-19 17:42:54 +1100

    fixed dynamic forms

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,14 +1,19 @@
 const addProd = function(e){
-  const addProdButton = document.getElementById('addProd');
   const addingProducts = document.getElementById('adding-products');
-  const forms = document.getElementById('forms')
-  const newForms = 0;
-  addProdButton.addEventListener("click", function() {
-      forms.append("<div><%= render partial: 'sales/form', locals: { form: form } %></div>");
-      console.log("newForms");
-  })
+  const container = document.getElementById('form-container');
+  const forms = document.getElementById('form');
+  const newForms = forms.cloneNode(true);
+  container.append(newForms);
+  console.log(newForms);
 }
 
+const addProdClick = function(e){
+  const addProdButton = document.getElementById('addProd');
+  addProdButton.addEventListener("click", function() {
+      addProd()
+  })
+};
+
 document.addEventListener("turbolinks:load", function() {
-  addProd();
+  addProdClick();
 })
